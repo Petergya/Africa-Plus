125e196fc54347a6cf6c8997131084109baac492
petergya <petergyang82@gmail.com>
2024-07-29 19:14:18 +0100

    update

--- @types.ts
+++ @types.ts
@@ -1,47 +1,4 @@
-// declare module 'react-native-datepicker' {
-//     import { Component } from 'react';
-//     import { ViewStyle, TextStyle } from 'react-native';
-  
-//     export interface DatePickerCustomStyles {
-//       dateInput?: ViewStyle;
-//       dateText?: TextStyle;
-//       placeholderText?: TextStyle;
-//       dateIcon?: ViewStyle;
-//       dateTouchBody?: ViewStyle;
-//       datePicker?: ViewStyle;
-//       datePickerMask?: ViewStyle;
-//       datePickerCon?: ViewStyle;
-//       disabled?: ViewStyle;
-//     }
-  
-//     export interface DatePickerProps {
-//       style?: ViewStyle;
-//       date?: string | Date;
-//       mode?: 'date' | 'datetime' | 'time';
-//       format?: string;
-//       minDate?: string | Date;
-//       maxDate?: string | Date;
-//       confirmBtnText?: string;
-//       cancelBtnText?: string;
-//       iconSource?: any;
-//       iconComponent?: React.ReactNode;
-//       duration?: number;
-//       customStyles?: DatePickerCustomStyles;
-//       showIcon?: boolean;
-//       hideText?: boolean;
-//       minuteInterval?: number;
-//       timeZoneOffsetInMinutes?: number;
-//       locale?: string;
-//       disabled?: boolean;
-//       onDateChange: (date: string, dateString: string) => void;
-//       placeholder?: string;
-//       androidMode?: 'default' | 'calendar' | 'spinner';
-//       is24Hour?: boolean;
-//       getDateStr?: (date: Date) => string;
-//     }
-  
-//     export default class DatePicker extends Component<DatePickerProps> {}
-//   }
+
 
 declare module 'react-native-datepicker' {
     import { Component } from 'react';
--- screenComponents/password/Login2.tsx
+++ screenComponents/password/Login2.tsx
@@ -1,49 +1,4 @@
-// import React from 'react';
-// import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
-// import tw from "tailwind-react-native-classnames";
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RootStackParamList } from '../../types';
 
-//
-
-// type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
-
-// type Props = {
-//   navigation: LoginScreenNavigationProp;
-// };
-
-// export default function Login2({ navigation }: Props) {
-//     function setEmail(_text: string): void {
-//         throw new Error('Function not implemented.');
-//     }
-
-//   return (
-//     <View style={tw`flex-1 justify-center items-center`}>
-//         <Image
-//         source={passImage}
-//         style={tw`h-64 w-full `}
-//         resizeMode="contain"
-//       />
-//       <View>
-//         <Text style={tw`text-2xl font-extrabold text-white `}>Forgot Password</Text>
-//         <Text style={tw`text-center text-white`}>Enter the email attached to your account.</Text>
-//       </View>
-//       <TextInput
-//         style={tw`w-80 border-b-2 border-gray-300 mb-4 p-2 text-white`}
-//         placeholder="Email"
-//         placeholderTextColor="white"
-//         value={email}
-//         onChangeText={setEmail}
-//       />
-//       <TouchableOpacity
-//         style={tw`bg-yellow-500 py-4 px-4 rounded-full mt-4`}
-//         onPress={() => navigation.navigate('Login3')}
-//       >
-//         <Text style={tw`text-white font-semibold px-20`}>Forgot Password</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
 import React, { useState } from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
