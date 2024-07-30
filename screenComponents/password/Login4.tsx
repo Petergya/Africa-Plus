3a484e020681e153c192c07b7ce7c3ae613843ed
petergya <petergyang82@gmail.com>
2024-07-30 11:28:26 +0100

    update

--- screenComponents/password/Login4.tsx
+++ screenComponents/password/Login4.tsx
@@ -1,32 +1,3 @@
-
-// import React from 'react';
-// import { View, Text, TouchableOpacity } from 'react-native';
-// import tw from "tailwind-react-native-classnames";
-// import { StackNavigationProp } from '@react-navigation/stack';
-// import { RootStackParamList } from '../../types'; 
-
-// type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Login'>;
-
-// type Props = {
-//   navigation: LoginScreenNavigationProp;
-// };
-
-// export default function Login({ navigation }: Props) {
-//   return (
-//     <View style={tw`flex-1 justify-center items-center`}>
-//       <View>
-//         <Text style={tw`text-2xl font-extrabold text-white`}>Welcome Back</Text>
-//       </View>
-//       <TouchableOpacity
-//         style={tw`bg-yellow-500 py-4 px-4 rounded-full mt-4`}
-//         onPress={() => navigation.navigate('Home')}
-//       >
-//         <Text style={tw`text-white font-semibold px-20`}>FINISH</Text>
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
-
 import React from 'react';
 import { View, Text, Image, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
